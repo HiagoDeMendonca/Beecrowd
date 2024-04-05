b0f9ff44d6228c1fc69b3cf1809e4817e35078eb
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-04-05 17:35:05 -0300

    Create CalculoSimples1010.java

---
+++ java/CalculoSimples1010.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+	
+	static DecimalFormat dfor = new DecimalFormat("0.00");
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		float valorFinal = 0;
+		
+		for (int i = 0; i < 2; i++){
+    		
+    		String input = scan.nextLine();
+    		String[] pecas = input.split(" ");
+    		
+    		valorFinal += Integer.parseInt(pecas[1]) * Float.parseFloat(pecas[2]);
+		}
+		
+		System.out.println("VALOR A PAGAR: R$ "+ dfor.format(valorFinal));
+		
+		scan.close();
+		
+	}
+	
+}
