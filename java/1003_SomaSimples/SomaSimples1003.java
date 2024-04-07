1c460b4925b1a918657301093bc3301956df5a72
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-04-07 17:36:37 -0300

    Folder creation and closing of the scanner

---
+++ java/1003_SomaSimples/SomaSimples1003.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner meuScanner = new Scanner(System.in);
+		
+		int input1 = meuScanner.nextInt();
+		int input2 = meuScanner.nextInt();
+		
+		System.out.println("SOMA = " + (input1 + input2));
+		
+		scan.close();
+		
+	}
+	
+}
