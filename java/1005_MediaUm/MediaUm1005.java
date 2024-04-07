d685f32b64e04676485612fde5063300fadb3181
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-04-07 17:53:08 -0300

    Folder creation, scanner update, closing of scanner and file name correction

--- java/1005_MediaUm/MediaUm1005.java
+++ java/1005_MediaUm/MediaUm1005.java
@@ -4,17 +4,18 @@ import java.text.DecimalFormat;
 public class Main {
 	
 	static DecimalFormat dfor = new DecimalFormat("0.00000");
+	static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		
-		Scanner meuScanner = new Scanner(System.in);
-		
-		double numeroA = meuScanner.nextDouble();
-		double numeroB = meuScanner.nextDouble();
+		double numeroA = scan.nextDouble();
+		double numeroB = scan.nextDouble();
 		
 		double mediaPond = (numeroA * 3.5 + numeroB * 7.5) / 11;
 		
 		System.out.println("MEDIA = " + dfor.format(mediaPond));
+		
+		scan.close();
 		
 	}
 	
