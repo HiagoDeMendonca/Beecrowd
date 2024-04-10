869ad38f73a5a7fe7446edb551876726f862f899
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-04-10 10:43:21 -0300

    Create

---
+++ java/1037_Intervalo/Intervalo1037.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		double input = scan.nextDouble();
+        
+		if (0 <= input && input <= 100)
+		{
+			int[] arrayIntervalos = {25, 50, 75, 100};
+			
+			int qual = 0;
+			
+			for (int i = 0; i < 4; i++)
+			{
+				if (input > arrayIntervalos[i]) {qual++;};
+			}
+			
+			String caractere = "(";
+			if (qual == 0) {caractere = "[";};
+			
+			System.out.println("Intervalo " + caractere + (arrayIntervalos[qual] - 25) + "," + arrayIntervalos[qual] + "]");
+			    
+		}
+		else
+		{
+			System.out.println("Fora de intervalo");
+		}
+		
+	}
+	
+}
