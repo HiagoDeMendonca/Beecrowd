fab3164e4a98043845f491685100976f6e52466e
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-04-10 10:55:27 -0300

    Scanner update

--- java/1006_MediaDois/MediaDois1006.java
+++ java/1006_MediaDois/MediaDois1006.java
@@ -4,14 +4,13 @@ import java.text.DecimalFormat;
 public class Main {
 	
 	static DecimalFormat dfor = new DecimalFormat("0.0");
+	static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		
-		Scanner meuScanner = new Scanner(System.in);
-		
-		double numeroA = meuScanner.nextDouble();
-		double numeroB = meuScanner.nextDouble();
-		double numeroC = meuScanner.nextDouble();
+		double numeroA = scan.nextDouble();
+		double numeroB = scan.nextDouble();
+		double numeroC = scan.nextDouble();
 		
 		double mediaPond = (numeroA * 2 + numeroB * 3 + numeroC * 5) / 10;
 		
