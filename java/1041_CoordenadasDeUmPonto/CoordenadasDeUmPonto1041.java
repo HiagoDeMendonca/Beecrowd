462279cf5b489a60eb5ffe02a3e900203d75f0e0
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-04-11 09:55:04 -0300

    Create

---
+++ java/1041_CoordenadasDeUmPonto/CoordenadasDeUmPonto1041.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		float X = scan.nextFloat();
+		float Y = scan.nextFloat();
+
+		int caso = 0;
+		
+		if (X > 0 && Y > 0) {caso = 1;}
+		else if (X < 0 && Y > 0) {caso = 2;}
+		else if (X < 0 && Y < 0) {caso = 3;}
+		else if (X > 0 && Y < 0) {caso = 4;}
+		else if (X == 0 && Y == 0) {caso = 5;}
+		else if (X == 0) {caso = 6;}
+		else if (Y == 0) {caso = 7;};
+		
+		if (caso <= 4) {System.out.println("Q" + caso);}
+		else
+		{
+			switch (caso)
+			{
+				case 5:
+					System.out.println("Origem");
+					break;
+				case 6:
+					System.out.println("Eixo Y");
+					break;
+				case 7:
+					System.out.println("Eixo X");
+					break;
+			}
+		}
+	}
+}
