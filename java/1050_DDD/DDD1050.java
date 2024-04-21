3317716e0ae78f9e8dc272e5cc13406760f74095
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-04-21 17:35:15 -0300

    Indentation correction

--- java/1050_DDD/DDD1050.java
+++ java/1050_DDD/DDD1050.java
@@ -7,39 +7,39 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int varDDD = scan.nextInt();
-			String cidade = "cidadeGenerica";
-			
-			switch (varDDD) {
-				case 61:
-					cidade = "Brasilia";
-					break;
-				case 71:
-					cidade = "Salvador";
-					break;
-				case 11:
-					cidade = "Sao Paulo";
-					break;
-				case 21:
-					cidade = "Rio de Janeiro";
-					break;
-				case 32:
-					cidade = "Juiz de Fora";
-					break;
-				case 19:
-					cidade = "Campinas";
-					break;
-				case 27:
-					cidade = "Vitoria";
-					break;
-				case 31:
-					cidade = "Belo Horizonte";
-					break;
-				default:
-					cidade = "DDD nao cadastrado";
-			}
-			
-			System.out.println(cidade);
-
+		String cidade = "cidadeGenerica";
+		
+		switch (varDDD) {
+			case 61:
+				cidade = "Brasilia";
+				break;
+			case 71:
+				cidade = "Salvador";
+				break;
+			case 11:
+				cidade = "Sao Paulo";
+				break;
+			case 21:
+				cidade = "Rio de Janeiro";
+				break;
+			case 32:
+				cidade = "Juiz de Fora";
+				break;
+			case 19:
+				cidade = "Campinas";
+				break;
+			case 27:
+				cidade = "Vitoria";
+				break;
+			case 31:
+				cidade = "Belo Horizonte";
+				break;
+			default:
+				cidade = "DDD nao cadastrado";
+		}
+		
+		System.out.println(cidade);
+		
 		scan.close();
 		
 	}
