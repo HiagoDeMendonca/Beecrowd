7700a8bd668307567e103a68487d2734bfaf5270
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-07-18 15:26:00 -0300

    Create

---
+++ java/1182_ColunaNaMatriz/ColunaNaMatriz1182.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+
+	static Scanner scan = new Scanner(System.in);
+	static DecimalFormat dfor = new DecimalFormat("0.0");
+
+	public static void main(String[] args) {
+		
+		float[][] valores = new float[12][12];
+		
+		byte coluna = scan.nextByte();
+		char operacao = scan.next().charAt(0);
+		
+		for (byte y = 0; y < 12; y++) {
+			for (byte x = 0; x < 12; x++) {
+				valores[x][y] = scan.nextFloat();
+			}
+		}
+		
+		double soma = 0;
+		
+		for (byte y = 0; y < 12; y++) {
+			soma += valores[coluna][y];
+		}
+		
+		if (operacao == 'M') {
+			System.out.println(dfor.format(soma / 12));
+		} else {
+			System.out.println(dfor.format(soma));
+		}
+		
+		scan.close();
+		
+	}
+}
