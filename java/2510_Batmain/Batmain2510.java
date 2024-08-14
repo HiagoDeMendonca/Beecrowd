aeefcdc84d87321dc85fabfd5d65a01aedf57e46
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-08-14 18:16:03 -0300

    Create

---
+++ java/2510_Batmain/Batmain2510.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int repeticoes = Integer.parseInt(scan.nextLine());
+		
+		for (int i = 0; i < repeticoes; i++) {
+			
+			scan.nextLine();
+			System.out.println("Y");
+			
+		}
+		
+		scan.close();
+		
+	}
+	
+}
