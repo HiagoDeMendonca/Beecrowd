414481b63c78617763660e19531dc5bdd66e4194
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-08-14 18:12:56 -0300

    Create

---
+++ java/2483_FelizNataaal/FelizNataaal2483.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int felicidade = scan.nextInt();
+		
+		System.out.print("Feliz nat");
+		
+		for (int i = 0; i < felicidade; i++) {
+			
+			System.out.print("a");
+			
+		}
+		
+		System.out.println("l!");
+		
+		scan.close();
+		
+	}
+}
