bd4b9c0c51e061c50a879c049c04730546b8177a
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-09-03 14:32:27 -0300

    Create

---
+++ java/3157_VivaACiencia/VivaACiencia3157.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		scan.nextLine();
+		System.out.println("Nao se refuta ciencia com opiniao");
+		
+		scan.close();
+		
+	}
+}
